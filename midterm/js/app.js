794ffbd0c27f883cb4faf08844a67cd572d98bea
JoJoPerez <JosephPerez@Josephs-MacBook-Pro-2.local>
2016-10-25 11:19:46 -0400

    reivsed

--- midterm/js/app.js
+++ midterm/js/app.js
@@ -1,25 +1,43 @@
 //console.log('connected js');
 
-document.addEventListener("DOMContentLoaded", function(event) {
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) { // waits for DOM
 
     
-    var Sneakers = document.getElementById('Sneakers');
+    var sneakers = document.getElementById('sneakers'); //get the divs and set to vars
+    var SB = document.getElementById('SB'); //just the button
+    
 
     
-    Sneakers.addEventListener("click", function(Sneakers){
-
- });   	
 
 
-
-
-var Blazers = function(coolSneakers) {
     
-    document.getElementById('displaySneakersDivId').innerHTML = "Sneakers I Like"
+    SB.addEventListener("click", function(){
+    	console.log('was clicked');
+    	sneakers.style.display = 'inline';
+    })
 
-}
 
-console.log(coolSneakers);
+	var colors = [ 'black','maroon','black & white', 'TCQ'];
+	for (i = 0; i < colors.length; i++) { 
+	    console.log (colors[i]);
+	    document.getElementById("colors").innerHTML += "<li>" + colors[i] + "</li>";
+
+	}
+ });   	// waits for DOM
+
+
+
+
+// var Blazers = function(coolSneakers) {
+    
+//     document.getElementById('displaySneakersDivId').innerHTML = "Sneakers I Like"
+
+// }
+
+// console.log(coolSneakers);
 
 
 
