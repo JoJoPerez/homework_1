99bdc7bcbaaf8bf8ea19988ab3fd8505176ddca0
JoJoPerez <JosephPerez@Josephs-MBP-2.home>
2016-10-25 01:24:29 -0400

    Midterm

---
+++ midterm/js/app.js
@@ -0,0 +1,30 @@
+//console.log('connected js');
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    
+    var Sneakers = document.getElementById('Sneakers');
+
+    
+    Sneakers.addEventListener("click", function(Sneakers){
+
+ });   	
+
+
+
+
+var Blazers = function(coolSneakers) {
+    
+    document.getElementById('displaySneakersDivId').innerHTML = "Sneakers I Like"
+
+}
+
+console.log(coolSneakers);
+
+
+
+
+
+
+
+
