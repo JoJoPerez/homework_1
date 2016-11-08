e155d404e37db183f06dfcb3ed6e369a98de2151
JoJoPerez <JosephPerez@Josephs-MBP-2.home>
2016-11-07 20:12:15 -0500

    Final Revised

--- Final_proj/js/app.js
+++ Final_proj/js/app.js
@@ -2,21 +2,52 @@
 
 $(function() {
 
-var enter = $('#enter');
-	
+	var enter = $('#enter');
+	var $thingsDiv = $('#things');
+
+	var $type = $('#type');
+	var $brand = $('#brand');
+	var $addShoe = $('#add-shoe');
 
 	$.ajax({
 		type: 'GET',
-		url: '/api/enter',
-		success: function(enter){
-			$.each(enter, funtion(i, enter) {
-			 $.enter.append('<li>name: '+ enter.name +', sneaker:'+ enter.sneaker +'</li>')
-			 });
-		});
+		url: 'http://rest.learncode.academy/api/johnbob/friends',
+		success: function(data){
+			console.log(data); // success statement 1 everything is OK
+		//	alert("API is UP safe to proceed");
 
+			$.each(data, function(i, thing){ // success statement 2
+				$thingsDiv.append("<li>" + thing.name + " you are this old: " + thing.age + "</li>")
+			});
+		},
+		error: function() {
+			alert("API is down /shrugz");
+		}
+	}); 
 
+	$addShoe.on('click', function(){
+		console.log('submit was clicked');
 
-     }); 
+		var shoe = {
+			name: $type.val(),
+			age: $brand.val()
+		}
+
+		console.log(shoe);
+	//	$thingsDiv.append("<li>" + shoe.type + " from: " + shoe.brand + "</li>");
+
+		$.ajax({
+			type: 'POST',
+			url: 'http://rest.learncode.academy/api/johnbob/friends',
+			data: shoe,
+			success: function(newThing) {
+				$thingsDiv.append("<li>" + newThing.name + " from: " + newThing.age + "</li>");
+			},
+			error: function() {
+				alert("problem adding POST REQ");
+			}
+		})
+	})
 });
 
 
